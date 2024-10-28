d9b9e13e72d40c2027b0ccccf036fea10e54a0b6
boyu <upboyu@gmail.com>
2024-10-28 11:22:45 +0800

    清理项目

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" visible />
+    <el-table-column prop="name" label="Name" width="180" :visible="false" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
 </template>
+
+<script setup>
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+</script>
