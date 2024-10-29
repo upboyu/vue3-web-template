ca957252039cb81278700d7860003c1fa90f20e8
boyu <upboyu@gmail.com>
2024-10-29 11:10:49 +0800

    格式化配置 storageToken封装

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,30 @@
+export function storageToken() {
+  const params = new URLSearchParams(
+    window.location.search || window.location.hash.split('?').slice(1).join('&'),
+  )
+  const token = params.get('X-Token')
+  const clientId = params.get('X-ClientId')
+  const event = params.get('event')
+  const source = params.get('source')
+
+  if (token && clientId) {
+    // 存储地址栏参数
+    localStorage.setItem('X-Token', token)
+    localStorage.setItem('X-ClientId', clientId)
+
+    // 移除地址栏参数
+    const url = new URL(window.location.href)
+    url.search = ''
+    url.hash = url.hash.split('?')[0]
+
+    window.history.replaceState({}, '', url.href)
+  }
+
+  // 存储 event source
+  localStorage.setItem('event', event)
+  localStorage.setItem('source', source)
+}
+
 export function pxToReal(designPx) {
   const designWidth = 1920 // 设计稿
   const deviceWidth = window.innerWidth // 设备
