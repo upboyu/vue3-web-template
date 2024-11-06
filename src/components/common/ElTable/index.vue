e83bf4b247363c62d1f71f0ae2185b06bb57b1d7
boyu <upboyu@gmail.com>
2024-11-06 09:34:19 +0800

    代码优化 增加可读性

--- src/components/common/ElTable/index.vue
+++ src/components/common/ElTable/index.vue
@@ -28,8 +28,8 @@ const { columnsState } = toRefs(props)
 const emit = defineEmits(['columnsStateChange'])
 
 const tableRef = ref()
-const defaultColumns = reactive([]) // 默认设置
-const columns = // 当前设置
+const defaultColumns = reactive([]) // 初始列状态
+const columns = // 本地列状态
   props.columnsStorageKey === undefined
     ? ref([])
     : useStorage(`columnsStorage:${location.href}-${props.columnsStorageKey}`, [])
@@ -40,40 +40,36 @@ provide('columns', columns)
 // 防止页面闪烁
 watch(columns, () => nextTick(() => tableRef.value.doLayout()), { deep: true })
 
-// 将内部数据同步到外部
-function handleCheckboxChange() {
-  emit('columnsStateChange', columns.value)
-}
+// 复选框变化时，将 columns 同步到外部
+const handleCheckboxChange = () => emit('columnsStateChange', columns.value)
 
-// 将外部数据同步到内部
-watch(columnsState, () => columnsStateToLocal(), { deep: true })
-function columnsStateToLocal() {
-  let _columnsState = columnsState.value
-  // 兼容 string
-  if (typeof _columnsState === 'string') {
-    try {
-      _columnsState = JSON.parse(_columnsState)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  if (!Array.isArray(_columnsState)) return // 处理错误格式
-  _columnsState.forEach(({ label, visible }) => {
-    const column = columns.value.find(item => item.label === label)
-    column && !column.disabled && (column.visible = visible)
-  })
-}
-
-// defaultColumns 初始化完成后，合并到 columns
+// 初始化完成后 将外部列状态和初始列状态合并到 columns
 nextTick(() => {
   columns.value = defaultColumns.map(col => ({
     ...col,
     visible: columns.value.find(item => item.label === col.label)?.visible ?? col.visible,
   }))
 
-  columnsStateToLocal() // 兼容 columnsState 为直接赋值的非响应式对象的时候
+  syncColumnsState()
 })
+
+// 持续监听外部列状态，将其同步到 columns
+watch(columnsState, syncColumnsState, { deep: true })
+
+function syncColumnsState() {
+  let _columnsState
+  try {
+    _columnsState =
+      typeof columnsState.value === 'string' ? JSON.parse(columnsState.value) : columnsState.value // 兼容 string
+  } catch (error) {
+    console.error(error)
+  }
+  if (!Array.isArray(_columnsState)) return // 处理错误格式
+  _columnsState.forEach(({ label, visible }) => {
+    const column = columns.value.find(item => item.label === label)
+    column && !column.disabled && (column.visible = visible)
+  })
+}
 </script>
 
 <style lang="scss" scoped>
